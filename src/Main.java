41d8b253e923f9856fa7430e0dd3e8fea921a919
Thibault Larzabal <thibault.larzabal@etu.unice.fr>
2017-03-01 14:26:57 +0100

    architecture

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
